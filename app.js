80473938123ef287dfc8c00b6dfc8366bc5b9603
Tahmid Alvee <151709545+tahmidalvee6@users.noreply.github.com>
2026-05-08 10:30:40 +0600

    Update app.js

---
+++ app.js
@@ -0,0 +1,153 @@
+function fetch_product() {
+  fetch("https://fakestoreapi.com/products")
+    .then((response) => response.json())
+    .then((data) => {
+      const container = document.getElementById("product-grid");
+      data.forEach((product) => {
+        card = document.createElement("div");
+        card.classList.add("product-card-wrapper");
+        card.innerHTML = ` 
+                <div class="product-card bg-white rounded-2xl border border-gray-200 overflow-hidden hover:shadow-lg">
+                    <img src="${product.image}" class="mx-auto h-56 object-contain p-4" alt="shirt">
+                    <div class="p-5">
+                        <p class="text-sm font-medium text-gray-900 mb-3">${product.title}</p>
+                        <div class="flex justify-between items-center">
+                            <span class="text-base font-bold text-gray-900">$${product.price}</span>
+                            <button id='btn-${product.id}' onclick="addToCart(${product.id}, ${product.price})" class="text-white bg-orange-500 rounded-lg px-4 py-2 text-xs font-semibold hover:bg-gray-900">Add To Cart</button>
+                        </div>
+                    </div>
+                </div>
+            `;
+        container.appendChild(card);
+      });
+    });
+}
+fetch_product();
+
+let balance = 1000;
+let dele_charge = 120;
+let isdiscount = false;
+let discount = 10;
+let initialcart = 0;
+let total_cost = 0;
+let total_cart = 0;
+
+let total_bal = document.querySelector(".total-bal");
+let add_price = document.getElementById("money-input");
+let final_bal = document.getElementById("final-bal");
+let cart_price = document.getElementById("cart-money");
+let add_money_btn = document.getElementById("add-money");
+let show_discount = document.getElementById("dis-show");
+let placeOrder = document.getElementById("place-order");
+let cupon_input = document.getElementById("cupon-input");
+let add_cupon_btn = document.getElementById("add-cupon");
+let cartCount = document.getElementById("cart-count");
+
+total_bal.innerHTML = balance;
+final_bal.innerHTML = balance;
+
+function addToCart(id, price) {
+  let btn = document.getElementById("btn-" + id);
+  if (btn.innerText == "Add To Cart") {
+    total_cart += 1;
+    let newCart = initialcart + price;
+    let discountAmount = 0;
+    if (isdiscount) {
+      discountAmount = (newCart * discount) / 100;
+    }
+    let totalCost = newCart + dele_charge - discountAmount;
+    if (totalCost > balance) {
+      alert("Warning! Not enough balance.");
+      return;
+    }
+    initialcart = newCart;
+    cart_price.innerHTML = initialcart;
+    // alert("Successfully added cart!");
+    btn.innerText = "Remove From Cart";
+    btn.classList.remove("bg-orange-500");
+    btn.classList.add("bg-red-500");
+    cartCount.innerText = total_cart;
+    calculation();
+  } else {
+    total_cart -= 1;
+    initialcart = initialcart - price;
+    cart_price.innerHTML = initialcart;
+    btn.innerText = "Add To Cart";
+    btn.classList.remove("bg-red-500");
+    btn.classList.add("bg-orange-500");
+    cartCount.innerText = total_cart;
+    calculation();
+    // alert("Remove From Cart");
+  }
+}
+
+add_money_btn.addEventListener("click", function () {
+  const inputValue = parseFloat(add_price.value);
+
+  if (isNaN(inputValue) || inputValue == "") {
+    alert("Please enter a valid amount");
+    return;
+  }
+  balance += inputValue;
+  total_bal.innerHTML = balance;
+  add_price.value = "";
+  calculation();
+});
+
+add_cupon_btn.addEventListener("click", function () {
+  const inputValue = cupon_input.value;
+  if (inputValue == "SMART10") {
+    isdiscount = true;
+    alert("cupon applied");
+    calculation();
+  } else {
+    isdiscount = false;
+    alert("invalid cupon code!");
+  }
+});
+function calculation() {
+  let discountAmount = 0;
+  if (isdiscount) {
+    discountAmount = (initialcart * discount) / 100;
+  }
+  let totalCost = initialcart + dele_charge - discountAmount;
+  let finalTotal = balance - totalCost;
+  if (finalTotal < 0) {
+    finalTotal = 0;
+  }
+  final_bal.innerHTML = totalCost;
+}
+
+placeOrder.addEventListener("click", function () {
+  if (initialcart == 0) {
+    alert("your cart is empty!");
+    return;
+  }
+  let discountAmount = 0;
+  if (isdiscount) {
+    discountAmount = (initialcart * discount) / 100;
+  }
+  let totalCost = initialcart + dele_charge - discountAmount;
+
+  if (totalCost > balance) {
+    alert("Warning! Not enough balance.");
+    return;
+  }
+  balance = balance - totalCost;
+  total_bal.innerText = balance;
+
+  let allButtons = document.querySelectorAll('button[id^="btn-"]');
+  for (let i = 0; i < allButtons.length; i++) {
+    allButtons[i].innerText = "Add To Cart";
+    allButtons[i].classList.remove("bg-red-500");
+    allButtons[i].classList.add("bg-orange-500");
+  }
+
+  initialcart = 0;
+  cart_price.innerText = "0";
+  isdiscount = false;
+  cupon_input.value = "";
+
+  calculation();
+  alert("Order placed successfully! Thank you for shopping!");
+});
